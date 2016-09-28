78147bd28ec1c2837a1965e00b7f0b0b60317ad9
U-EU\mihai.dragan <mihai.dragan@AIL-PC0035.eu.autoliv.int>
2016-09-28 15:46:06 +0100

    updated comment header

--- src/descriptors-register.adb
+++ src/descriptors-register.adb
@@ -1,23 +1,16 @@
 ------------------------------------------------------------------------------
 --                                                                          --
---                          SVD Binding Generator                           --
+--                     TIXML2Ada Binding Generator                          --
 --                                                                          --
---                    Copyright (C) 2015-2016, AdaCore                      --
+-- * based on SVD2Ada released by AdaCore in 2016 under GPL 3 license       --
 --                                                                          --
--- SVD2Ada is free software;  you can  redistribute it  and/or modify it    --
+-- This is free software;  you can  redistribute it  and/or modify it       --
 -- under terms of the  GNU General Public License as published  by the Free --
 -- Software  Foundation;  either version 3,  or (at your option) any later  --
--- version.  SVD2Ada is distributed in the hope that it will be useful, but --
--- WITHOUT ANY WARRANTY;  without even the  implied warranty of MERCHANTA-  --
--- BILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public  --
--- License for  more details.  You should have  received  a copy of the GNU --
--- General Public License  distributed with SVD2Ada; see file COPYING3.  If --
--- not, go to http://www.gnu.org/licenses for a complete copy of the        --
+-- version. Go to http://www.gnu.org/licenses for a complete copy of the    --
 -- license.                                                                 --
 --                                                                          --
 ------------------------------------------------------------------------------
-
-with System;
 
 -- XML dependencies
 with DOM.Core;          use DOM.Core;
@@ -270,13 +263,13 @@ package body Descriptors.Register is
             Add_Aspect (Rec, "Volatile_Full_Access");
             Add_Size_Aspect (Rec, Reg.Reg_Properties.Size);
 
-            if Reg.Reg_Properties.Endianess = Big_Endian then
-               Add_Bit_Order_Aspect (Rec, System.High_Order_First);
-               Add_Scalar_Storage_Order_Aspect (Rec, System.High_Order_First);
-            elsif Reg.Reg_Properties.Endianess = Little_Endian then
-               Add_Bit_Order_Aspect (Rec, System.Low_Order_First);
-               Add_Scalar_Storage_Order_Aspect (Rec, System.Low_Order_First);
-            end if;
+--              if Reg.Reg_Properties.Endianess = Big_Endian then
+--                 Add_Bit_Order_Aspect (Rec, System.High_Order_First);
+--                 Add_Scalar_Storage_Order_Aspect (Rec, System.High_Order_First);
+--              elsif Reg.Reg_Properties.Endianess = Little_Endian then
+--                 Add_Bit_Order_Aspect (Rec, System.Low_Order_First);
+--                 Add_Scalar_Storage_Order_Aspect (Rec, System.Low_Order_First);
+--              end if;
 
             declare
                Res : Ada_Type'Class := Simplify (Rec, Spec);
